98d4b09eae0d5ec7563461c64bbeeb4a67c40fab
Adil ali <adil.ali_cs22@gla.ac.in>
2026-06-29 13:13:31 +0530

    Search input added

--- FirebaseContactApp/src/Components/Search.jsx
+++ FirebaseContactApp/src/Components/Search.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MdPersonSearch } from "react-icons/md";
+import { FaUserPlus } from "react-icons/fa6";
 
 function Search() {
   return (
     <>
-    <div>
-      <input type="text" placeholder=''/>
-    </div>
+      <div className="relative flex items-center px-5">
+        <MdPersonSearch className="ml-2 text-2xl absolute text-white" />
+        <input
+          type="text"
+          placeholder="Search Contacts"
+          className="h-10 grow rounded-md border border-white bg-transparent pl-10"
+        />
+        <div>
+            <FaUserPlus className="ml-2 text-2xl text-white cursor-pointer"/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
