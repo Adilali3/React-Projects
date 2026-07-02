b20fb8e3c29c15818a10a2c140fa5751aa68560d
Adil ali <adil.ali_cs22@gla.ac.in>
2026-07-02 10:16:50 +0530

    added useState and useEffect hooks for contacts

--- FirebaseContactApp/src/App.jsx
+++ FirebaseContactApp/src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Search from './Components/Search'
+import {collection, getDocs} from "firebase/firestore"
+import { db } from "./config/Firebase";
+import ContactCard from './Components/ContactCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () =>{
+      try {
+        const contactRef = collection(db, "contacts")
+        const contactSnapshot = await getDocs(contactRef)
+        const contactList = contactSnapshot.docs.map((doc) =>({
+          id:doc.id,
+          ...doc.data(),
+        }))   
+        setContacts(contactList);
+      } catch (error) {
+        console.log("erorr")  
+      }
+    };
+
+    getContacts();
+   
+  },[])
+
 
   return (
     <>
@@ -15,6 +38,14 @@ function App() {
      <Navbar />
     </div>
     <Search />
+    <div className='gap-3'>
+      {
+        contacts.map((contact)=>(
+          <ContactCard key={contact.id}
+          contact={contact} />
+        ))
+      }
+    </div>
     </>
   )
 }
